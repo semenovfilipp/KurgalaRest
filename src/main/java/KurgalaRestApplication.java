0cac2fc1c549da96e9b4cdde7f1376efdc19d08f
semenovfilipp <znexiss@gmail.com>
2023-04-24 21:58:39 +0300

    24.04 - исправление полей

--- src/main/java/KurgalaRestApplication.java
+++
@@ -1,8 +0,0 @@
-
-
-@SpringBootApplication
-public class KurgalaRestApplication {
-    public static void main (String [] args){
-        StringApplication.run(KurgalaRestApplication.class, args);
-    }
-}
--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -1,4 +1,4 @@
-package controller;
+package org.example.controller;
 
 @Controller
 public class MainController {
